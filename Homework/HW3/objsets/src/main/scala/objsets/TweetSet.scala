2afb1c56dff39cdaf5f02a5054b7c145b20739b7
Checkwhei Sin <Checkwhei Sin>
2016-05-30 22:57:07 -0400

    part 4

--- Homework/HW3/objsets/src/main/scala/objsets/TweetSet.scala
+++ Homework/HW3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -9,6 +9,8 @@ class Tweet(val user: String, val text: String, val retweets: Int) {
   override def toString: String =
     "User: " + user + "\n" +
     "Text: " + text + " [" + retweets + "]"
+
+  def mentions(keywords: List[String]): Boolean = keywords.exists(x => text.contains(x))
 }
 
 /**
@@ -68,6 +70,11 @@ abstract class TweetSet {
     def tail: TweetSet
 
     def isEmpty: Boolean
+
+
+    def mentions(keywords: List[String]): TweetSet = {
+      filter(x => x.mentions(keywords))
+    }
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -243,14 +250,14 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.mentions(google)
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.mentions(apple)
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+     lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
   }
 
 object Main extends App {
