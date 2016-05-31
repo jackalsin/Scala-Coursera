6066ec4a6d572faba2ebb6ee5e7a25e6548711f9
Checkwhei Sin <Checkwhei Sin>
2016-05-30 21:38:03 -0400

    finish part 2

--- Homework/HW3/objsets/src/main/scala/objsets/TweetSet.scala
+++ Homework/HW3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,8 +54,20 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
-  
+    def union(that: TweetSet): TweetSet = {
+      def unionHelper(tweetSet: TweetSet, acc: TweetSet): TweetSet = {
+        if (tweetSet.isEmpty) acc
+        else unionHelper(tweetSet.tail, acc.incl(tweetSet.head))
+      }
+
+      unionHelper(that, this)
+    }
+
+    def head: Tweet
+
+    def tail: TweetSet
+
+    def isEmpty: Boolean
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -77,7 +89,10 @@ abstract class TweetSet {
    * and be implemented in the subclasses?
    */
     def descendingByRetweet: TweetList = ???
-  
+
+
+
+  // ---------------------------------------------------------------------------
   /**
    * The following methods are already implemented
    */
@@ -107,7 +122,7 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
   
   /**
    * The following methods are already implemented
@@ -120,12 +135,24 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  override def head: Tweet = throw new NoSuchElementException("Empty.head")
+
+  override def tail: TweetSet = throw new NoSuchElementException("Empty.tail")
+
+  override def isEmpty: Boolean = true
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+      val otherHit = right.filterAcc(p, left.filterAcc(p, acc))
+
+      if (p(elem)) otherHit incl elem
+      else otherHit
+
+    }
+
     
   /**
    * The following methods are already implemented
@@ -152,6 +179,18 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     left.foreach(f)
     right.foreach(f)
   }
+
+  override def head: Tweet = {
+    if (left.isEmpty) elem
+    else left.head
+  }
+
+  override def tail: TweetSet = {
+    if (left.isEmpty) right
+    else new NonEmpty(elem, left.tail, right)
+  }
+
+  override def isEmpty: Boolean = false
 }
 
 trait TweetList {
